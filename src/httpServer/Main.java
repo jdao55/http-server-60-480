26767b6c65ce1cad1d18228bc7d56e232f646835
jdao55 <joedao55@gmail.com>
2019-01-16 20:41:14 -0500

    added cli args & added image file support

--- src/httpServer/Main.java
+++ src/httpServer/Main.java
@@ -18,34 +18,42 @@ public class Main {
     //TODO add site rootPath, and port number as commandline args
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 
-        server = new ServerSocket(port);
-        Scanner in = new Scanner(System.in);
         FileUtil.rootPath=System.getProperty("user.dir");
 
-        while(true){
-            Socket socket =  server.accept();
+        try {
+            parseCliArgs(args);
+            server = new ServerSocket(port);
+            Scanner in = new Scanner(System.in);
 
-            BufferedReader inStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                
-            String input,info;
-            Map<String,String> header = new HashMap<String,String>();
+            while (true) {
+                Socket socket = server.accept();
 
-            //read Request
-            info = inStream.readLine();
-            input = inStream.readLine();
-            while(input.length()>0) {         
-            	String[] tokens = input.split(":",2);                               		
-                header.put(tokens[0], tokens[1]);
+                BufferedReader inStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                String input, info;
+                Map<String, String> header = new HashMap<String, String>();
+
+                //read Request
+                info = inStream.readLine();
                 input = inStream.readLine();
+                while (input.length() > 0) {
+                    String[] tokens = input.split(":", 2);
+                    header.put(tokens[0], tokens[1]);
+                    input = inStream.readLine();
+                }
+
+                HTTPRequest request = new HTTPRequest(info, header);
+
+                //process request
+                handleRequest(request, socket);
+
+                inStream.close();
+                socket.close();
             }
-                  
-            HTTPRequest request = new HTTPRequest(info,header);
-
-            //process request
-            handleRequest(request, socket);
-
-            inStream.close();
-            socket.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
         }
    
 
@@ -69,15 +77,35 @@ public class Main {
     private static void handleGet(HTTPRequest req, Socket soc) throws IOException
     {
         try {
-            String body = FileUtil.readFile(req.getURL(), Charset.forName("UTF-8"));
+            byte [] body = FileUtil.readFileBytes(req.getURL());
             HTTPResponse resp = new HTTPResponse(body, 200);
             soc.getOutputStream().write(resp.getHeaderBytes());
         }
         catch (FileNotFoundException e)
         {
-            HTTPResponse resp = new HTTPResponse("<h>404 Not Found</h>", 404);
+            HTTPResponse resp = new HTTPResponse("<h>404 Not Found</h>".getBytes(), 404);
             soc.getOutputStream().write(resp.getHeaderBytes());
         }
 
+    }
+
+    private static void parseCliArgs(String[] args) throws Exception
+    {
+        for(int i=0;i< args.length;i++)
+        {
+            if (args[i].matches("(-d)|(-dir)"))
+            {
+                if (i+1==args.length)
+                    throw new Exception("Invalid args");
+                FileUtil.rootPath=args[++i];
+            }
+            if (args[i].matches("(-p)|(-port)"))
+            {
+                if (i+1==args.length)
+                    throw new Exception("Invalid args");
+                port = Integer.parseInt(args[++i]);
+            }
+
+        }
     }
 }
--- src/httpUtil/HTTPResponse.java
+++ src/httpUtil/HTTPResponse.java
@@ -11,15 +11,15 @@ public class HTTPResponse {
     private int httpVersionMajor;
     private int httpVersionMinor;
     private int statusCode;
-    private String body;
+    private byte[] body;
     private HashMap<String, String> headerMap;
 
-    public HTTPResponse(String _body, int _statusCode) {
+    public HTTPResponse(byte[] _body, int _statusCode) {
         body = _body;
         httpVersionMajor = 1;
         httpVersionMinor = 1;
         headerMap = new HashMap<String, String>();
-        headerMap.put("Content-Length", Integer.toString(body.length()));
+        headerMap.put("Content-Length", Integer.toString(body.length));
         headerMap.put("Date", getServerTime());
         statusCode = _statusCode;
 
@@ -31,25 +31,31 @@ public class HTTPResponse {
     }
 
     public byte[] getHeaderBytes() {
-        String resp = String.format("HTTP/%d.%d %d %s\r\n", httpVersionMajor, httpVersionMinor, statusCode, getStatusString(statusCode));
+        String respHeader = String.format("HTTP/%d.%d %d %s\r\n", httpVersionMajor, httpVersionMinor, statusCode, getStatusString(statusCode));
         for (Map.Entry<String, String> entry : headerMap.entrySet()) {
             String key = entry.getKey();
             String value = entry.getValue();
-            resp += key + ": " + value + "\r\n";
+            respHeader += key + ": " + value + "\r\n";
         }
-        resp += "\r\n" + body;
-        return resp.getBytes();
+        respHeader += "\r\n";
+
+        //create header byte array from header and body
+        byte[] respHeaderBytes=respHeader.getBytes();
+        byte[] resp= new byte[respHeaderBytes.length+body.length];
+        System.arraycopy(respHeaderBytes,0, resp,0, respHeaderBytes.length);
+        System.arraycopy(body,0, resp, respHeaderBytes.length, body.length);
+        return resp;
     }
 
     public String getHeader(String key) {
         return headerMap.get(key);
     }
 
-    public String getBody() {
+    public byte[] getBody() {
         return body;
     }
 
-    public void setBody(String _b) {
+    public void setBody(byte[] _b) {
         body = _b;
     }
 
